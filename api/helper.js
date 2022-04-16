3609abdfce87a7eb0369b0bcb12bbb1f1452f229
Pomatech <pomatechlead@gmail.com>
2022-04-16 16:33:22 -0500

    Add getHarvestCost api

--- api/helper.js
+++ api/helper.js
@@ -1,3 +1,5 @@
+var url = require('url');
+
 function helperApi(app, db, accounts, helperContract) {
     app.get('/houseDetails/:id', async (req, res) => {
         const id = req.params.id;
@@ -5,6 +7,14 @@ function helperApi(app, db, accounts, helperContract) {
         res.json(data);
     });
 
+    app.get('/harvestCost/:id', async (req, res) => {
+        const id = req.params.id;
+        const harvestingReward = req.query.harvestingReward;
+        console.log(req.query.harvestingReward);
+        const data = await helperContract.methods.getHarvestCost(id, harvestingReward).call();
+        res.json(data);
+    });
 }
 
 module.exports = helperApi
+   
