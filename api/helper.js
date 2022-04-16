10d10c28a688828b7a8ca0cb45266e3b4cae7213
Pomatech <pomatechlead@gmail.com>
2022-04-16 09:42:37 -0500

    Integrate helper getHouseDetails function

--- api/helper.js
+++ api/helper.js
@@ -1,14 +1,8 @@
 function helperApi(app, db, accounts, helperContract) {
-    app.get('/contacts', async (request, response) => {
-        let cache = [];
-        const COUNTER = await gameContract.methods.count().call();
-
-        for (let i = 1; i <= COUNTER; i++) {
-            const contact = await gameContract.methods.contacts(i).call();
-            cache = [...cache, contact];
-        }
-
-        response.json(cache);
+    app.get('/houseDetails/:id', async (req, res) => {
+        const id = req.params.id;
+        const data = await helperContract.methods.getHouseDetails(id).call();
+        res.json(data);
     });
 
 }
