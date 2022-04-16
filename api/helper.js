e7df23e0472f395c16ed6f65155c6038fc1b4917
Pomatech <pomatechlead@gmail.com>
2022-04-16 16:35:51 -0500

    Add getRepairCost api

--- api/helper.js
+++ api/helper.js
@@ -1,17 +1,24 @@
 var url = require('url');
 
 function helperApi(app, db, accounts, helperContract) {
-    app.get('/houseDetails/:id', async (req, res) => {
+    app.get('/getHouseDetails/:id', async (req, res) => {
         const id = req.params.id;
         const data = await helperContract.methods.getHouseDetails(id).call();
         res.json(data);
     });
 
-    app.get('/harvestCost/:id', async (req, res) => {
+    app.get('/getHarvestCost/:id', async (req, res) => {
         const id = req.params.id;
         const harvestingReward = req.query.harvestingReward;
         console.log(req.query.harvestingReward);
         const data = await helperContract.methods.getHarvestCost(id, harvestingReward).call();
+        res.json(data);
+    });
+
+    app.get('/getRepairCost/:id', async (req, res) => {
+        const id = req.params.id;
+        const percent = req.query.percent
+        const data = await helperContract.methods.getRepairCost(id, percent).call();
         res.json(data);
     });
 }
