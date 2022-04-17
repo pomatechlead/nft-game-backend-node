a3b70d6da1be81eb52d9ec746417d399538ab350
Pomatech <pomatechlead@gmail.com>
2022-04-17 08:33:51 -0500

    Add getResource api

--- api/game.js
+++ api/game.js
@@ -1,5 +1,11 @@
 function helperApi(app, db, accounts, houseContract) {
-    
+    app.get('/getResource/:id', async (req, res) => {
+        const id = req.params.id;
+        const user = req.query.user;
+        const data = await houseContract.methods.getResource(user, id).call();
+        res.json(data);
+    });
+
 }
 
 module.exports = helperApi
