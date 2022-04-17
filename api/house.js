ba4987df6a61b2d1b8907f35e1f4b10e712b70da
Pomatech <pomatechlead@gmail.com>
2022-04-16 21:21:46 -0500

    Add getHouse api

--- api/house.js
+++ api/house.js
@@ -1,5 +1,9 @@
 function helperApi(app, db, accounts, houseContract) {
-    
+    app.get('/getHouse/:id', async (req, res) => {
+        const id = req.params.id;
+        const data = await houseContract.methods.getHouse(id).call();
+        res.json(data);
+    });
 }
 
 module.exports = helperApi
